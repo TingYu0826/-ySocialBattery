3f90bc5489109afab1036f387c1b8823f494bf6e
Ting Yu Chen <D1397193@o365.fcu.edu.tw>
2025-08-01 01:47:09 +0800

    Initial Commit

---
+++ ＭySocialBattery/_ySocialBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ySocialBatteryApp.swift
+//  ＭySocialBattery
+//
+//  Created by 陳亭瑜 on 2025/8/1.
+//
+
+import SwiftUI
+
+@main
+struct _ySocialBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ＭySocialBatteryTests/_ySocialBatteryTests.swift
@@ -0,0 +1,17 @@
+//
+//  _ySocialBatteryTests.swift
+//  ＭySocialBatteryTests
+//
+//  Created by 陳亭瑜 on 2025/8/1.
+//
+
+import Testing
+@testable import _ySocialBattery
+
+struct _ySocialBatteryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
